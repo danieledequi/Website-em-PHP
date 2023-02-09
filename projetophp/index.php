aca75a7ba306a035b5698beb5fea94f44a7d4423
danieledequi <danielefdequi@gmail.com>
2023-02-09 10:20:10 -0300

    segundo

---
+++ projetophp/index.php
@@ -0,0 +1,105 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Estudando FrontEnd</title>
+</head>
+<body> 
+    <h1>FrontEnd na prática.</h1>
+
+</br>
+
+<center>
+<h2>O que é desenvolvimento front-end? E o que faz um programador Front End?</h2>
+
+<img width="300px" height="150px"  src="giphy.gif">
+</center>
+
+<p>A área de desenvolvimento web front-end é a ponte que une os mundos do design e da programação. O profissional desenvolvedor front-end é o responsável por colocar em prática, através de códigos, o design de um site ou interface.
+
+Enquanto o web design projeta o visual de um site, o desenvolvimento front-end implementa esse design através de códigos, como HTML, CSS e JavaScript. Ou seja, é o front-end que coloca “para funcionar” toda a parte visual de uma interface, como a página de um site ou de um aplicativo.</p>
+    
+<br>
+<h4>Agora vamos estudar. Escolha sua linguagem de programação principal:</h4>
+<center>
+<form> 
+    <select name="cursos">
+        <option value= "">Selecione</option>
+        <option value= "html">HTML</option>
+        <option value= "css">CSS</option>
+        <option value= "JavaScript">JavaScript</option>
+        <option value= "react">React</option>
+        <option value= "jquery">JQuery</option>
+        </select>
+    <button class="bt" type="submit">Enviar</button>
+
+</h4>
+</center>
+</form>
+   
+<style type="text/css">
+    *{
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+    
+}
+h1{
+background-color:blue;
+color: white;
+padding: 15px 10px;
+text-align: center;
+font-family: monospace,serif;
+
+}
+h2{
+color: black;
+padding: 10px 10px;
+font-size: 20px
+font-family: verdana,serif;
+
+}
+p{
+color: black;
+padding: 8px 10px;
+font-size: 15px;
+border:1px solid white;
+font-family: verdana,serif
+Align="justify";
+
+}
+h4{
+color: black;
+padding: 8px 10px;
+font-size: 17px;
+border:1px solid white;
+margin-bottom: 1.5em;
+font-family: verdana,serif;
+
+}
+.bt{
+background-color:blue;
+color:white;
+width: 150px;
+height: 40px;
+border: 3px solid;
+border-radius: 15px;
+cursor:pointer;
+
+}
+select{
+width:150px;
+height:33px;
+border: 3px solid;
+border-radius: 10px;
+
+}
+
+
+</style>
+
+
+</body>
+</html>
